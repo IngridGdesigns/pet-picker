a3324ccb8917442fc6500b20a9ff9b78a96737bf
IngridGdesigns <garciai@protonmail.com>
2024-02-16 02:53:26 -0800

    added cat data and logging it in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// javascript
+import { catBreedData } from '/data.js'
+
+const options = document.getElementById("options");
+const catActivityForm = document.getElementById("catbreed-choice")
+
+catActivityForm
+
+function renderForm(){
+    console.log(catBreedData)
+}
+
+renderForm()
