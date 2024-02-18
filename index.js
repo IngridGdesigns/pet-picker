d3bd1eecea5c6ebe947c1713ad796c702b5eae43
IngridGdesigns <garciai@protonmail.com>
2024-02-17 22:21:37 -0800

    added image as logo, updated html

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import { catBreedData } from '/data.js'
 const options = document.getElementById("options");
 const catActivityForm = document.getElementById("catbreed-choice")
 
-catActivityForm
-
-function renderForm(){
+function renderForm() {
+    
     console.log(catBreedData)
 }
 
