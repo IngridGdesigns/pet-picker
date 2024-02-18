dafb118fd998be64c6dc0808b69f385c1afc8087
IngridGdesigns <garciai@protonmail.com>
2024-02-17 23:50:44 -0800

    using FormData to fetch values of form

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import { catBreedData } from '/data.js'
 
 const options = document.getElementById("options");
-const catActivityForm = document.getElementById("catbreed-choice")
+const catbreedForm = document.querySelector("#catbreed-choice")
+const formBtn = document.getElementById("submit")
 
-function renderForm() {
-    
-    console.log(catBreedData)
+formBtn.addEventListener("click", fetchFormData)
+
+function renderForm(){
+    // console.log(catBreedData)
+    // console.log("catBreedData")
 }
 
-renderForm()
+function fetchFormData(e){
+    e.preventDefault();
+    console.log('data fetched')
+    
+    const form = new FormData(catbreedForm)
+   
+    const values = [...form.entries()];
+    console.log(values)
+    
+    console.log(values.length)
+    // if(values.length === 2){
+    //     formBtn.disabled = false;
+    // }
+   
+}
+
+
